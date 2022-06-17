a2c761079ffd7a7b5915a43f935575eceb035def
Annie Huang <annie.hn@gmail.com>
2022-06-17 21:36:33 +1000

    Implement the function to open and close the modal. modal.showModal() and modal.close(), quite simple

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('#modal');
 const openModal = document.querySelector('.open-button');
 const closeModal = document.querySelector('.close-button');
+
+openModal.addEventListener('click', () => {
+  // show() will display the <dialog> in it's original place
+  // modal.show();
+
+  // showModal() will display the <dialog> like a modal, with shaded background and place the <dialog> into the center.
+  // When the modal open and when you click escape, it will close automatically, out of box.
+  // But click outside does not work.
+  modal.showModal();
+});
+
+closeModal.addEventListener('click', () => {
+  modal.close();
+});
